0820a53b43e3772795b30ad6f6bbdee7585a55b6
sauravkumar329 <46621822+sauravkumar329@users.noreply.github.com>
2023-06-06 21:16:49 -0700

    game dashboard page

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,13 +1,27 @@
 import React, { useEffect, useState } from "react";
 import Sidebar from "../sidebar/Sidebar";
-import { Card, Col, Row, Container } from "react-bootstrap";
+import { Card, Col, Row, Container, Alert, Button } from "react-bootstrap";
 import { Bar, Doughnut, Line, Pie } from "react-chartjs-2";
 import Chart from "chart.js/auto";
+import "./Dashboard.css";
+import { useNavigate } from "react-router-dom";
 
 const apiKey = "3896265182c3481ab09163b92a9cd5bd";
 const gameID = "fifa-22-xbox-one";
+
+const ratingToVariant = {
+  1: "danger",
+  2: "danger",
+  3: "warning",
+  4: "warning",
+  5: "success",
+};
 const Dashboard = () => {
-  const [game, setGame] = useState(null);
+  const [mostPopular, setMostPopular] = useState(null);
+  const [easports, setEAsports] = useState(null);
+  const [highRated, setHighRated] = useState(null);
+  // const [game, setGame] = useState(null);
+  const navigate = useNavigate();
   let backgroundColors = [
     "rgba(54, 162, 235, 0.8)",
     "rgba(255, 206, 86, 0.8)",
@@ -54,19 +68,46 @@ const Dashboard = () => {
     const fetchGameDetails = async () => {
       try {
         const response = await fetch(
-          `https://api.rawg.io/api/games?ordering=-rating&developers=109?key=${apiKey}`
+          `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-added&key=${apiKey}`
         );
         const data = await response.json();
         console.log(data);
-        setGame(data);
+        setMostPopular(data.results);
       } catch (error) {
         console.error("Error fetching game details:", error);
       }
     };
-
+    const EASportGames = async () => {
+      try {
+        const response = await fetch(
+          `https://api.rawg.io/api/games?ordering=-rating&developers=109&key=${apiKey}`
+        );
+        const data = await response.json();
+        console.log(data);
+        setEAsports(data.results);
+      } catch (error) {
+        console.error("Error fetching game details:", error);
+      }
+    };
+    const highestRatedGames = async () => {
+      try {
+        const response = await fetch(
+          `https://api.rawg.io/api/games?dates=2001-01-01,2001-12-31&ordering=-rating&key=${apiKey}`
+        );
+        const data = await response.json();
+        setHighRated(data.results);
+      } catch (error) {
+        console.error("Error fetching game details:", error);
+      }
+    };
+    highestRatedGames();
     fetchGameDetails();
+    EASportGames();
   }, []);
-  if (game === null) {
+  const showDetails = (element, id) => {
+    navigate("/gamedetails", { state: { gameData: element } });
+  };
+  if (mostPopular === null) {
     return <p>Loading...</p>;
   }
   // You may use different data for other charts
@@ -75,39 +116,137 @@ const Dashboard = () => {
     <div>
       <Sidebar />
       <Container>
-        <Row></Row>
-        {/* <Row>
-          <Col md={4}>
-            <Card>
+        <Row>
+          <h4 style={{ color: "white" }}>Welcome to Game Scout App</h4>
+        </Row>
+        <Row>
+          <Col lg={4}>
+            <Card className="scrollable-div" style={{ height: "500px" }}>
+              <Card.Header>Most popular games in 2019</Card.Header>
               <Card.Body>
-                <Bar data={data1} />
+                {mostPopular.map((item, index) => (
+                  <Alert
+                    onClick={() => showDetails(item)}
+                    key={index}
+                    className="alert-hover"
+                    style={{
+                      backgroundImage: `url(${item.background_image})`,
+                      color: "white",
+                    }}
+                  >
+                    <p>
+                      {item.name}{" "}
+                      <Button
+                        className="btn btn-sm"
+                        key={index}
+                        variant={ratingToVariant[Math.ceil(item.rating)]}
+                      >
+                        ★ {item.rating}
+                      </Button>
+                    </p>
+                    <hr />
+                    <p>Released : {item.released}</p>
+                    <p>
+                      Platforms :{" "}
+                      {item.platforms
+                        .map((platformItem, index) => (
+                          <span key={index}>{platformItem.platform.name}</span>
+                        ))
+                        .reduce((prev, curr, index) => [prev, ", ", curr])}
+                    </p>
+                    <p className="view-details" style={{ display: "none" }}>
+                      View Details
+                    </p>
+                  </Alert>
+                ))}
               </Card.Body>
             </Card>
           </Col>
-          <Col md={4}>
-            <Card>
+          <Col lg={4}>
+            <Card className="scrollable-div" style={{ height: "500px" }}>
+              <Card.Header>Highest rated game by Electronic Arts</Card.Header>
               <Card.Body>
-                <Doughnut data={data1} />
+                {easports.map((item, index) => (
+                  <Alert
+                    onClick={() => showDetails(item)}
+                    key={index}
+                    className="alert-hover"
+                    style={{
+                      backgroundImage: `url(${item.background_image})`,
+                      color: "white",
+                    }}
+                  >
+                    <p>
+                      {item.name}{" "}
+                      <Button
+                        className="btn btn-sm"
+                        key={index}
+                        variant={ratingToVariant[Math.ceil(item.rating)]}
+                      >
+                        ★ {item.rating}
+                      </Button>
+                    </p>
+                    <hr />
+                    <p>Released : {item.released}</p>
+                    <p>
+                      Platforms :{" "}
+                      {item.platforms
+                        .map((platformItem, index) => (
+                          <span key={index}>{platformItem.platform.name}</span>
+                        ))
+                        .reduce((prev, curr, index) => [prev, ", ", curr])}
+                    </p>
+                    <p className="view-details" style={{ display: "none" }}>
+                      View Details
+                    </p>
+                  </Alert>
+                ))}
               </Card.Body>
             </Card>
           </Col>
-          <Col md={4}>
-            <Card>
+          <Col lg={4}>
+            <Card className="scrollable-div" style={{ height: "500px" }}>
+              <Card.Header>Highest rated games from 2001</Card.Header>
               <Card.Body>
-                <Line data={data1} />
+                {highRated.map((item, index) => (
+                  <Alert
+                    onClick={() => showDetails(item)}
+                    key={index}
+                    className="alert-hover"
+                    style={{
+                      backgroundImage: `url(${item.background_image})`,
+                      color: "white",
+                    }}
+                  >
+                    <p>
+                      {item.name}{" "}
+                      <Button
+                        className="btn btn-sm"
+                        key={index}
+                        variant={ratingToVariant[Math.ceil(item.rating)]}
+                      >
+                        ★ {item.rating}
+                      </Button>
+                    </p>
+                    <hr />
+                    <p>Released : {item.released}</p>
+                    <p>
+                      Platforms :{" "}
+                      {item.platforms
+                        .map((platformItem, index) => (
+                          <span key={index}>{platformItem.platform.name}</span>
+                        ))
+                        .reduce((prev, curr, index) => [prev, ", ", curr])}
+                    </p>
+                    <p className="view-details" style={{ display: "none" }}>
+                      View Details
+                    </p>
+                  </Alert>
+                ))}
               </Card.Body>
             </Card>
           </Col>
         </Row>
-        <Row>
-          <Col md={12}>
-            <Card>
-              <Card.Body>
-                <Bar options={options} data={data1} />;
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row> */}
       </Container>
     </div>
   );
